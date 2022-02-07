000087fd62a90e3a49f7a0e93753758cb479f3fe
fran14arce <farcer01@educantabria.es>
2022-02-07 12:05:26 +0100

    Navbar creado

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { Navbar, Container, Nav } from 'react-bootstrap';
+import ElementosNavbar from '../data/ElementosNavbar';
 
 class Navbar extends React.Component {
   constructor(props) {
@@ -8,17 +10,16 @@ class Navbar extends React.Component {
   render() {
     return (
       <>
-        <ul>
-          <li>
-            <a href="#ej1">Ejercicio 1</a>
-          </li>
-          <li>
-            <a href="#ej2">Ejercicio 2</a>
-          </li>
-          <li>
-            <a href="#ej3">Ejercicio 3</a>
-          </li>
-        </ul>
+        <Navbar bg="dark" variant="dark">
+          <Container>
+            <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+            <Nav className="me-auto">
+              {ElementosNavbar.map((item) => {
+                <Nav.Link href={item.path}>{item.title}</Nav.Link>
+              })}
+            </Nav>
+          </Container>
+        </Navbar>
       </>
     );
   }
---
+++ src/data/ElementosNavbar.js
@@ -0,0 +1,31 @@
+import Ejercicio1 from '../components/Ejercicio1';
+import Ejercicio2 from '../components/Ejercicio2';
+import Ejercicio3 from '../components/Ejercicio3';
+import Ejercicio4 from '../components/Ejercicio4';
+
+export const ElementosNavbar = [
+  {
+    id: 1,
+    path: '../components/Ejercicio1',
+    title: 'Ejercicio 1',
+    component: Ejercicio1,
+  },
+  {
+    id: 2,
+    path: '../components/Ejercicio2',
+    title: 'Ejercicio 2',
+    component: Ejercicio2,
+  },
+  {
+    id: 3,
+    path: '../components/Ejercicio3',
+    title: 'Ejercicio 3',
+    component: Ejercicio3,
+  },
+  {
+    id: 4,
+    path: '../components/Ejercicio4',
+    title: 'Ejercicio 4',
+    component: Ejercicio4,
+  },
+];
